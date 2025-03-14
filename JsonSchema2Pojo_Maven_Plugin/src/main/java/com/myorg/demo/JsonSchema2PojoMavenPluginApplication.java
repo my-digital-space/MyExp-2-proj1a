c8c0de4a0759c6e884cc409a716f38304ddbcc5b
prosenjit005 <mydigitalspace.pro@gmail.com>
2025-03-14 16:36:41 +0530

    initial commit

---
+++ JsonSchema2Pojo_Maven_Plugin/src/main/java/com/myorg/demo/JsonSchema2PojoMavenPluginApplication.java
@@ -0,0 +1,13 @@
+package com.myorg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSchema2PojoMavenPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonSchema2PojoMavenPluginApplication.class, args);
+	}
+
+}
