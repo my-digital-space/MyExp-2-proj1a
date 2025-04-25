1915fc57b944c5a8d346f73a582f099869ad32ad
prosenjit005 <mydigitalspace.pro@gmail.com>
2025-04-25 12:51:38 +0530

    initial commit

---
+++ Code_Format_SB_Gradle_Spotless_plugin/src/main/java/com/demo/code_format/CodeFormatSbGradleSpotlessPluginApplication.java
@@ -0,0 +1,13 @@
+package com.demo.code_format;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFormatSbGradleSpotlessPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFormatSbGradleSpotlessPluginApplication.class, args);
+	}
+
+}
