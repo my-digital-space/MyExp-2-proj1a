5f3043a69655c898f08a542406345a7c9f2e7a16
prosenjit005 <mydigitalspace.pro@gmail.com>
2025-04-25 15:52:01 +0530

    initial commit

---
+++ Terraform_GCP_SpringBoot/GCP_PubSub_Pull_Subscription_SB_TF/src/main/java/com/gcp/pubsub_pull_subs/GcpPubSubPullSubscriptionSbTfApplication.java
@@ -0,0 +1,13 @@
+package com.gcp.pubsub_pull_subs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpPubSubPullSubscriptionSbTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpPubSubPullSubscriptionSbTfApplication.class, args);
+	}
+
+}
---
+++ Terraform_GCP_SpringBoot/GCP_PubSub_Pull_Subscription_SB_TF/src/test/java/com/gcp/pubsub_pull_subs/GcpPubSubPullSubscriptionSbTfApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.gcp.pubsub_pull_subs;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GcpPubSubPullSubscriptionSbTfApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
