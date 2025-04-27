484b83d4737d94deb5674e6349ac6164056f9e76
prosenjit005 <mydigitalspace.pro@gmail.com>
2025-04-27 13:42:37 +0530

    initial commit

---
+++ Junit/Junit_5_SB_RestClient/src/main/java/com/junit/restclient/Junit5SbRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.junit.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5SbRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5SbRestClientApplication.class, args);
+	}
+
+}
---
+++ Junit/Junit_5_SB_RestClient/src/test/java/com/junit/restclient/Junit5SbRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junit.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5SbRestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
